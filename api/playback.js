c250a6fff66a550af3e0cdf7fe0c8817aa7bf97a
gvjacob <gvjacob@outlook.com>
2020-02-01 14:12:42 -0500

    default track

--- api/playback.js
+++ api/playback.js
@@ -3,6 +3,12 @@ import { isEmpty, get } from 'lodash';
 import spotify, { withRefresh } from './_spotify';
 import { getTrackData } from './_utils';
 
+const DEFAULT_TRACK = {
+  name: "California Dreamin'",
+  artist: 'José Feliciano',
+  image: 'https://i.scdn.co/image/ab67616d00001e025f0a59daa1b8f11bfbfe94f3',
+};
+
 const getRecentlyPlayed = async () => {
   const { body } = await spotify.getMyRecentlyPlayedTracks({
     type: 'track',
@@ -10,7 +16,7 @@ const getRecentlyPlayed = async () => {
   });
 
   const recentlyPlayed = get(body, ['items', 0, 'track']);
-  return getTrackData(recentlyPlayed);
+  return recentlyPlayed ? getTrackData(recentlyPlayed) : DEFAULT_TRACK;
 };
 
 const getPlayback = withRefresh(async (req, res) => {
